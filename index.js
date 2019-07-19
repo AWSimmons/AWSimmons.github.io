aa3d0cfe8117499038a0f6b8dd4cdd1d46121edc
Andrew Simmons <andrew@simmons.co.uk>
2019-07-19 14:15:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class FrontPageViewModel {
     mainStory = {
-        imageSrc: 'mugshot.png',
+        imageSrc: 'mugshot.jpg',
         headline: 'Ghost suspect to face trial',
         story: `
         Esteban Gomez, 34, was today scheduled to face trial in New Angeles Central Criminal Court for nine counts of murder. Police, acting on a tip off from a private detective who asked to remain anonymous, were able to find and capture Gomez in his apartment near Quinde. NAPD spokesperson Angelica Comer stated that they have significant evidence linking Gomez to all eight of the "Ghost" murders as well as that of a New Angeles Sol journalist.
@@ -9,7 +9,7 @@ class FrontPageViewModel {
 
     sideStories = [
         {
-            imageSrc: 'serverjpg',
+            imageSrc: 'server.jpg',
             headline: 'Cybercrime on the rise as server farms hit in nightly raids',
             story: `
             A number of New Angelino server farms have been left embarassed as hackers have bypassed their security controls, deleting client data and hijacking their processing. Small business owners fear that since these attacks seem localised to New Angeles, it could be the work of protectionist foreign governments. 
