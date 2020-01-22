10aa7a2596e92e88396f073560527edcfa82c5f3
Andrew Simmons <andrew@simmons.co.uk>
2020-01-22 23:22:23 +0000

    Create 2-5.js

---
+++ editions/2-5.js
@@ -0,0 +1,25 @@
+export default {
+    mainStory: {
+        image: '251.jpg',
+        headline: 'Haas-Bioroid Arcology in Lockdown',
+        story: `
+            The Seisō arcology in San Salvador today entered a state of lockdown for unknown reasons. While communications were inconsistent, it is understood that none of the 40,000 persons in the building were harmed in any way and there is no ongoing emergency.
+            Designed by famed architect Taqi El Wahab to include a 5GW fusion reactor and containing hydroponic vegetable farms as well as fisheries producing luxuries from salmon to caviar, the Seisō building is capable of sustaining each of its 20,000 full time inhabitants as well as the employees of Haas Bioroid's second largest R&D and Engineering departments in South America. 
+            The Seisō arcology is owned by Seiso REIT, a fully owned subsidiary of Haas-Bioroid GMBH.
+    },
+    sideStories: [{
+        image: '252.jpg',
+        headline: `Another crime solved by clone detective`,
+        story: `
+            Caprice Nisei, the first Jinteki clone to serve in the NAPD, was today responsible for the arrest of alleged cyber-terrorist Jacob Isaacson. A Nisei-line clone equipped with superhuman skills of social judgment, Caprice marks a new high in NAPD-Jinteki cooperation. We look forward to a future where Jinteki helps keep New Angelinos even safer than they are now.
+        `
+    }, {
+        image: '253.jpg',
+        headline: 'Will 2x19 be the year of the Blockchain?',
+        story: `
+            Jinzu O'Hara, CEO of BitQ industries, announced yesterday that BitQ has developed technology that will allow avocado consumers to verify that their produce is not counterfeit by looking up a unique avocado ID on the blockchain.
+            BitQ stock rose slightly by 0.3% in the wake of the announcement, perhaps indicating a degree of fatigue amongst investors. Hedge fund manager Laramy Fisk said "Since the release of qubitcoin, everyone's been hyping up their blockchain technology and we haven't yet seen anyone make it economically viable. For full disclosure, I am short BitQ."
+        `
+    }],
+    date: '29/06/19'
+};
