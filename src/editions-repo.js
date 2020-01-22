2d708e52ecd167c452ff0743b2d5145f2d0f68d9
Andrew Simmons <andrew@simmons.co.uk>
2020-01-22 23:23:56 +0000

    adding to editions repo

--- src/editions-repo.js
+++ src/editions-repo.js
@@ -1,8 +1,9 @@
-import edition1 from '/editions/1.js';
-import edition2 from '/editions/2.js';
-import edition3 from '/editions/3.js';
-import edition4 from '/editions/4.js';
+import edition1   from '/editions/1.js';
+import edition2   from '/editions/2.js';
+import edition2p5 from '/editions/2-5.js';
+import edition3   from '/editions/3.js';
+import edition4   from '/editions/4.js';
 
-export const allEditions = [edition4, edition3, edition2, edition1];
+export const allEditions = [edition4, edition3, edition2p5, edition2, edition1];
 
 export const currentEdition = ko.observable(edition4);
