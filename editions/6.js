cfb8b390fde3ca5e6d36c78ab2343e5146fb1923
Andrew Simmons <andrew@simmons.co.uk>
2020-01-25 19:24:09 +0000

    Create 6.js

---
+++ editions/6.js
@@ -0,0 +1,24 @@
+  
+export default {
+    mainStory: {
+        image: '601.jpg',
+        headline: 'NAPD failing to turn arrests into convictions',
+        story: `
+            This month, NAPD arrest processing is down by 8% on a year-on-year basis. In an exclusive interview to the New Angeles Sol, an NAPD source pointed the finger at Haas-Bioroid, claiming that they are behind on Bioroid evidence processing.
+        `
+    },
+    sideStories: [{
+        image: '602.jpg',
+        headline: `Juba chosen as 2x22 Olympic host city`,
+        story: `
+          The Jinteki International Olympic Committee revealed today that Juba is to be the host city for the 2x22 Olympics, beating out entries from Chinese Taipei and from Almaty. TIOC officials stated they were hugely impressed with the standards of the sporting venues, but refused to comment on the human rights record of the government of South Sudan.
+    `
+    }, {
+        image: '603.jpg',
+        headline: 'Could this year's El Niño lead to higher energy costs?',
+        story: `
+           The El Niño weather pattern has led to storms in the pacific ocean which have disrupted Deuterium shipping to the Chile and New Angeles. Markets predict a 4% increase in per-unit electricity costs as a result of the shortage.
+    `
+    }],
+    date: '08/07/19'
+};
